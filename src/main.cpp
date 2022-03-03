c21e7a092ef4769f5f2b28a02545a094bb4cc826
Kevin Tan <tankevin@stanford.edu>
2022-03-03 03:16:00 -0800

    ✨ Add on/off line detection

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <Metro.h>
 
+// Miscellaneous
+#define BAUD_RATE 9600
+
 // Motor Pin Definitions
 #define MOTOR_R_ENABLE 2
 #define MOTOR_R_DIR_1 3
@@ -15,24 +18,39 @@
 bool status_led_is_on = true;
 Metro status_led_timer = Metro(STATUS_LED_TIME_INTERVAL_MS);
 
+// Infrared Reflectance Sensor Definitions
+#define LINE_THRESHOLD_CL 800
+#define LINE_THRESHOLD_CC 830
+#define LINE_THRESHOLD_CR 870
+#define IR_SENSOR_CL 18 // Center Left
+#define IR_SENSOR_CC 16 // Center Center
+#define IR_SENSOR_CR 14 // Center Right
+
 // Function Prototypes
 void initialize_motor_pins();
 void motor_r_forward();
 void motor_l_forward();
 bool test_status_led_timer_expired();
-bool resp_status_led_timer_expired();
+void resp_status_led_timer_expired();
+bool test_on_line();
+void resp_on_line();
+void resp_off_line();
 
 void setup()
 {
+  Serial.begin(BAUD_RATE);
   initialize_motor_pins();
-  motor_r_forward();
-  motor_l_forward();
 }
 
 void loop()
 {
   if (test_status_led_timer_expired())
     resp_status_led_timer_expired();
+  if (test_on_line())
+    resp_on_line();
+  else
+    resp_off_line();
+  delay(500);
 }
 
 void initialize_motor_pins()
@@ -64,9 +82,24 @@ bool test_status_led_timer_expired()
   return status_led_timer.check();
 }
 
-bool resp_status_led_timer_expired()
+void resp_status_led_timer_expired()
 {
   status_led_timer.reset();
   status_led_is_on = not status_led_is_on;
   digitalWrite(STATUS_LED_PIN, status_led_is_on);
+}
+
+bool test_on_line()
+{
+  return analogRead(IR_SENSOR_CC) < LINE_THRESHOLD_CC;
+}
+
+void resp_on_line()
+{
+  Serial.println("On");
+}
+
+void resp_off_line()
+{
+  Serial.println("Off");
 }
